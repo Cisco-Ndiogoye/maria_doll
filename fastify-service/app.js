5d4e93e3c2fdcf71d3d2a72073372f97838a8052
FilipCisco <ndiogoye.cisco@outlook.com>
2021-06-02 16:26:04 +0000

    code smells fixing 2

--- fastify-service/app.js
+++ fastify-service/app.js
@@ -1,7 +1,6 @@
 const fastify = require('fastify')(
     { logger: true }    
-)
-//fastify.register(require('fastify-multipart'));
+);
 const routes = require('./routes')
 fastify.register(routes)
 fastify.register(require('fastify-cors'), { 
--- fastify-ui/src/components/ProductItem.vue
+++ fastify-ui/src/components/ProductItem.vue
@@ -49,7 +49,6 @@ export default {
       type: String,
       default: '#'
     }
-  },
-  methods() {  }
+  }
 }
 </script>
